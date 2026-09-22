e612b0e45d5d5fd77a188e5211c5fe47925385b6
ruiacabral-star <rui.a.cabral@gmail.com>
2026-09-22 12:23:59 +0100

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// build.gradle.kts (Raiz do Projeto)
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("kotlin-kapt") version "1.9.22" apply false workaround -- remove version if needed, or use below:
+    id("kotlin-kapt") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
